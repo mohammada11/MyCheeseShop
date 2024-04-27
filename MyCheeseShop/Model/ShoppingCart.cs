2878610052a0297689a41c588733d2670d00b4b9
Ansar Ahmad <mohammad.a.ahmad@icloud.com>
2024-04-27 15:38:16 +0100

    Currently working on finishing the cart summary page

--- MyCheeseShop/Model/ShoppingCart.cs
+++ MyCheeseShop/Model/ShoppingCart.cs
@@ -2,7 +2,43 @@
 {
     public class ShoppingCart
     {
-        public List<ShoppingCart> Items { get; set; } = new List<ShoppingCart>();
+        public event Action? OnCartUpdated;
+        private List<CartItem> _items;
 
+        public ShoppingCart()
+        {
+            _items = [];
+        }
+
+        public void AddItem(Cheese cheese, int quantity)
+        {
+            var item = _items.FirstOrDefault(item => item.Cheese.Id == cheese.Id);
+            if (item == null)
+                _items.Add(new CartItem { Cheese = cheese, Quantity = quantity });
+            else
+                 item.Quantity += quantity;
+
+
+            OnCartUpdated?.Invoke();
+        }
+
+        public IEnumerable<CartItem> GetItems()
+        {
+            return _items;
+        }
+
+        public int GetQuantity(Cheese cheese)
+        {
+            // return the quantity of the cheese in the cart
+            var item = _items.FirstOrDefault(item => item.Cheese.Id == cheese.Id);
+            return item?.Quantity ?? 0;
+        }
+
+        public void SetItems(IEnumerable<CartItem> items)
+        {
+            // set the items in the cart
+            _items = items.ToList();
+            OnCartUpdated?.Invoke();
+        }
     }
 }
