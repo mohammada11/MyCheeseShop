0b0f87346d46096d2fad9f6b24b28a3bea333d46
Ansar Ahmad <mohammad.a.ahmad@icloud.com>
2024-05-01 10:12:01 +0100

    finished the action buttons

--- MyCheeseShop/Model/ShoppingCart.cs
+++ MyCheeseShop/Model/ShoppingCart.cs
@@ -49,5 +49,28 @@
             _items = items.ToList();
             OnCartUpdated?.Invoke();
         }
+
+        public void RemoveItem(Cheese cheese)
+        {
+            var item = _items.RemoveAll(item => item.Cheese.Id ==  cheese.Id);
+            OnCartUpdated?.Invoke();
+
+        }
+
+        public void RemoveItem(Cheese cheese, int quantity)
+        {
+            var item = _items.FirstOrDefault(item => item.Cheese.Id == cheese.Id);
+            if(item is not null)
+            {
+                item.Quantity = quantity;
+                if(item.Quantity <= 0)
+                _items.Remove(item);
+            }
+            OnCartUpdated?.Invoke();
+
+
+        }
+
+
     }
 }
